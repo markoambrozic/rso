57cc601eb12dea07d66d25162da20f6722af1e0b
Marko Ambrožič <marko.ambrozic@studio-moderna.com>
2018-12-31 14:41:44 +0100

    Project updates

--- catalog/src/main/java/com/kumuluz/ee/samples/microservices/simple/ProductsResource.java
+++ catalog/src/main/java/com/kumuluz/ee/samples/microservices/simple/ProductsResource.java
@@ -1,6 +1,9 @@
 package com.kumuluz.ee.samples.microservices.simple;
 
+import com.kumuluz.ee.logs.LogManager;
+import com.kumuluz.ee.logs.Logger;
 import com.kumuluz.ee.samples.microservices.simple.Models.Product;
+import com.kumuluz.ee.samples.microservices.simple.Interceptors.LogContextInterceptor;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
@@ -10,25 +13,28 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
-
+import com.kumuluz.ee.logs.cdi.Log;
+import com.kumuluz.ee.logs.cdi.LogParams;
 
 @Path("/products")
 @RequestScoped
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Log
 public class ProductsResource {
 
     @PersistenceContext
     private EntityManager em;
 
+    private static final Logger LOG = LogManager.getLogger(ProductsResource.class.getName());
 
     @GET
+    @Log
     public Response getProducts() {
-
         TypedQuery<Product> query = em.createNamedQuery("Product.findAll", Product.class);
 
         List<Product> products = query.getResultList();
-
+        
         return Response.ok(products).build();
     }
 
@@ -36,12 +42,12 @@ public class ProductsResource {
     @Path("/{id}")
     public Response getProduct(@PathParam("id") Integer id) {
 
-        Product b = em.find(Product.class, id);
+        Product p = em.find(Product.class, id);
 
-        if (b == null)
+        if (p == null)
             return Response.status(Response.Status.NOT_FOUND).build();
 
-        return Response.ok(b).build();
+        return Response.ok(p).build();
     }
 
     @POST
@@ -56,5 +62,36 @@ public class ProductsResource {
         em.getTransaction().commit();
 
         return Response.status(Response.Status.CREATED).entity(p).build();
+    }
+
+    @PUT
+    public Response updateProduct(Product p) {
+
+        int id = p.getId();
+
+        em.getTransaction().begin();
+
+        Product pUpdate = em.find(Product.class, id);
+        pUpdate.setName(p.getName());
+        pUpdate.setDescription(p.getDescription());
+        pUpdate.setPrice(p.getPrice());
+
+        em.persist(pUpdate);
+
+        em.getTransaction().commit();
+
+        return Response.status(Response.Status.FOUND).entity(pUpdate).build();
+    }
+
+    @DELETE
+    public Response deleteProduct(Product p) {
+        em.getTransaction().begin();
+
+        p = em.find(Product.class, p.getId());
+        em.remove(p);
+
+        em.getTransaction().commit();
+
+        return Response.status(Response.Status.OK).build();
     }
 }
