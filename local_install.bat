f99a367c308f456b3a7680e3d2f630a993e7afe3
Marko Ambrožič <marko.ambrozic@studio-moderna.com>
2019-01-03 20:44:20 +0100

    added local install script

---
+++ local_install.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/markoambrozic/cart.git
+git clone https://github.com/markoambrozic/catalog.git
+git clone https://github.com/markoambrozic/orders.git
---
+++ local_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/markoambrozic/cart.git
+git clone https://github.com/markoambrozic/catalog.git
+git clone https://github.com/markoambrozic/orders.git
