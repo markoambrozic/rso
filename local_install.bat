2cbec39c711977641f3d4dd41240119c818a3e20
Lamp <dmvoicestudios@gmail.com>
2019-01-06 21:06:10 +0100

    Included install settings for new services

--- local_install.bat
+++ local_install.bat
@@ -1,3 +1,5 @@
 git clone https://github.com/markoambrozic/cart.git
 git clone https://github.com/markoambrozic/catalog.git
 git clone https://github.com/markoambrozic/orders.git
+git clone https://github.com/LampDM/dashboard.git
+git clone https://github.com/LampDM/emailing.git
