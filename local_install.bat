06cf8b87f958f40d4734f74e006d3c911c01663c
Lamp <dmvoicestudios@gmail.com>
2019-01-07 11:45:23 +0100

    Added journaling mc installation

--- local_install.bat
+++ local_install.bat
@@ -3,3 +3,4 @@ git clone https://github.com/markoambrozic/catalog.git
 git clone https://github.com/markoambrozic/orders.git
 git clone https://github.com/LampDM/dashboard.git
 git clone https://github.com/LampDM/emailing.git
+git clone https://github.com/LampDM/journaling.git
